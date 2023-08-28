05a3b638c3d530f89fcc408ba5bdbef9f406c06c
j21winter <j21winter@gmail.com>
2023-08-27 23:26:36 -0400

    tidying up

--- baby_clothing_size_guide/app/controllers/suggestion_controller.py
+++ baby_clothing_size_guide/app/controllers/suggestion_controller.py
@@ -5,6 +5,4 @@ from flask import request, redirect, render_template
 @app.route('/suggestion/search', methods=['POST'])
 def search_suggestions():
     Suggestion.get_search_suggestions(request.form)
-    # Suggestion.get_search_suggestions_for_height(request.form)
-    # Suggestion.get_search_suggestions_for_weight(request.form)
     return redirect(f'/child/show/{request.form["child_id"]}')
